30220c47570fd6bc8dff38ef50fd7151225f9c7a
Hilal38 <hhilalkocak@gmail.com>
2024-03-07 09:17:40 +0300

    Add node.js stack

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require('./mongo-connection')
+
 
 const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+app.set('view engine', 'pug')
+
 
 module.exports = app
+
